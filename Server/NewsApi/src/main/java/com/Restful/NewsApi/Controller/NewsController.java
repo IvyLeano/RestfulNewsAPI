bea31b89275e6f3ac75fbae6fb06c7ef370e24b5
Ivy Leano <IvyLeano@outlook.com>
2020-09-30 17:01:10 -0400

    Add CrossOrigin decorator to NewsController to prevent CORS error in flutter web version

--- Server/NewsApi/src/main/java/com/Restful/NewsApi/Controller/NewsController.java
+++ Server/NewsApi/src/main/java/com/Restful/NewsApi/Controller/NewsController.java
@@ -5,6 +5,7 @@ import com.Restful.NewsAPI.Service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class NewsController {
         this.newsService = newsService;
     }
 
+    @CrossOrigin
     @GetMapping("/source{source}")
     public ResponseEntity<NewsResponse> getNewsBySource(@RequestParam("source") String source) {
         try {
@@ -33,6 +35,7 @@ public class NewsController {
         }
     }
 
+    @CrossOrigin
     @GetMapping("/category{category}")
     public ResponseEntity<NewsResponse> getNewsByCategory(@RequestParam("category") String category) {
         try {
